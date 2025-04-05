7a0f0d5b7cfe49245b10f32fa0731b39bc608bcb
oliviertremblay <olivier.tremblay.10@gmail.com>
2025-04-04 20:29:23 -0400

    Ajoutez des fichiers projet.

---
+++ QuoteMachine-ExerciceGit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Bienvenue dans QuoteMachine!");
+Console.WriteLine("Ce projet est collaboratif et utilise le GitHub Flow.");
+Console.WriteLine("Développez une application console pour gérer et afficher des citations inspirantes.");
+Console.WriteLine();
+Console.WriteLine("Prochaines étapes : implémentez les fonctionnalités dans des branches distinctes.");
---
+++ QuoteMachine-ExerciceGit/QuoteManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteMachine_ExerciceGit
+{
+    public class QuoteManager
+    {
+        private List<Quote> quotes;
+
+        public QuoteManager()
+        {
+            quotes = new List<Quote>
+            {
+                new Quote { Text = "Le succès, c’est d’aller d’échec en échec sans perdre son enthousiasme.", Author = "Winston Churchill" },
+                new Quote { Text = "Soyez vous-même, tous les autres sont déjà pris.", Author = "Oscar Wilde" },
+                new Quote { Text = "La vie, c’est comme une bicyclette, il faut avancer pour ne pas perdre l’équilibre.", Author = "Albert Einstein" }
+            };
+        }
+
+        // À compléter avec des méthodes comme :
+        // public Quote GetRandomQuote()
+        // public void AddQuote(string text, string author)
+    }
+}
